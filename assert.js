1312ce865574e6ae0d911521ed28bd372b065835
RyanOrsinger <ryan.orsinger@gmail.com>
2019-08-21 10:54:56 -0500

    update assertion function to compare collections as well as primitives.

--- assert.js
+++ assert.js
@@ -1,6 +1,9 @@
 // Keep this function definition in order to test expected vs. actual results
 const assert = function(actual, expected, message = "") {
-    if (actual !== expected) {
+
+    // This compares primitive values and collections. If they differ, throw an error.
+    if(JSON.stringify(actual) !== JSON.stringify(expected)) {
         throw Error("Assert failed. The expected and actual values are not the same. " + message);
     }
+    
 };
