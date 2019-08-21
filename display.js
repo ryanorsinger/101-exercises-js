443ec56a26dba9b2aec137125996f231d574da26
RyanOrsinger <ryan.orsinger@gmail.com>
2019-08-21 10:54:50 -0500

    update addToDone function

--- display.js
+++ display.js
@@ -1,5 +1,5 @@
 // creates and appends a list item to the page's section to show number correct.
-function addToDoneList(message) {
+function addToDone(message) {
     var node = document.createElement("LI");                 // Create a <li> node
     var textnode = document.createTextNode(message);         // Create a text node
     node.appendChild(textnode);                              // Append the text to <li>
