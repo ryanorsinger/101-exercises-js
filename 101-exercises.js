30143dc3c3859d6401f14ea58cdb030176357305
RyanOrsinger <ryan.orsinger@gmail.com>
2019-08-21 10:37:48 -0500

    add welcome

--- 101-exercises.js
+++ 101-exercises.js
@@ -1,5 +1,5 @@
+// Welcome to 101 Exercises in JS
 // If you get "ReferenceError: someVariableName is not defined", that means you need to create a variable
-// If youi get a "TypeError" of any kind, it means that there is a data type mismatch. Check the data types.
 
 // Exercise #0, Example Problem:
 // Example problem setup: Create a variable named doingJSRightNow and assign it the boolean true.
