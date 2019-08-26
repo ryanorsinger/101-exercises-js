aa498b74cfdace2958277a41e43e1f3139a55a20
Glitch (101-exercises) <none>
2019-08-26 18:28:12 +0000

    Replace #84, increase information in assertion message

--- display.js
+++
@@ -1,9 +0,0 @@
-// creates and appends a list item to the page's section to show number correct.
-function addToDone(message) {
-    var node = document.createElement("LI");                 // Create a <li> node
-    var textnode = document.createTextNode(message);         // Create a text node
-    node.appendChild(textnode);                              // Append the text to <li>
-    node.classList.add("finished");
-    document.querySelector(".correct ul").appendChild(node);
-    document.querySelector("#count").innerHTML = document.querySelectorAll('.finished').length + " of 101";
-}
